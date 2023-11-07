a13f3f4e6805c00d539c0db0a4ddbb44a96dbec3
Dennis Rand <rand@ecrime.dk>
2023-11-07 22:21:32 +0100

    - Moved from json config to yaml - Added some schema validation.

--- app/config.py
+++ app/config.py
@@ -1,24 +1,70 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import json
 import os 
 import sys
+import glob
+import yaml
+import yamale
+from yamale.validators import DefaultValidators, Validator, Regex
 
-def loadConfig() -> dict:
+def validateSiteConfigs():
+    schemaFile = os.path.join('sites', 'sites.schema')
+    sitesYaml = glob.glob(os.path.join('sites', "*.yaml"))
+    for file in sitesYaml:
+        validated = schemaValidator(file, schemaFile)
+        if not (validated):
+            raise Exception(file + " Schema Validation failed!")        
+
+def schemaValidator(yamlFile: str, schemaFile: str) -> bool:
+    """ Performs some validation of the schema related to the yaml files
+    :param yamlFile: The yaml file to be validated
+    :param schemaFile: the schema to validate against
+    return: bool: Return True if ok, else return False
+    """
+    validators = DefaultValidators.copy() 
+    schema = yamale.make_schema(schemaFile, validators=validators)
+    data = yamale.make_data(yamlFile)
     try:
-        with open(os.path.join('config', 'config.json'), 'r') as f:
-            configCore = json.load(f)
-    except:
-        print ("config.json failed to load")
+        yamale.validate(schema, data)
+        return(True)
+    except ValueError as e:
+        print('Validation failed!\n%s' % str(e))
+        return(False)
+
+def loadConfigYaml() -> dict:
+    """ Loading the static config files related to the core Cratos FastAPI
+        return: Returns a dict with with content of config.yml and mappings.yml this is a merge of the two files.
+    """
+
+    # Handle parsing and error checking of config.yml
+    try:
+        with open(os.path.join('config', 'config.yaml'), 'r') as f:
+            configCore = yaml.safe_load(f)
+        validated = schemaValidator(os.path.join('config', 'config.yaml'), os.path.join('config', 'config.schema'))
+        if not (validated):
+            raise Exception("Schema Validation failed!")
+    except Exception as e:
+        # Handle other exceptions
+        print("An error occurred while attempting to load config.yaml")
+        print(f"Error message: {str(e)}")
         sys.exit(1)
+
+    # Handle parsing and error checking of mappings.yml
     try:
-        with open(os.path.join('config', 'mappings.json'), 'r') as f:
-            mappingsCore = json.load(f)
-    except:
-        print ("config.json failed to load")
+        with open(os.path.join('config', 'mappings.yaml'), 'r') as f:
+            mappingsCore = yaml.safe_load(f)
+        validated = schemaValidator(os.path.join('config', 'mappings.yaml'), os.path.join('config', 'mappings.schema'))
+        if not (validated):
+            raise Exception("Schema Validation failed!")
+        
+    except Exception as e:
+        # Handle other exceptions
+        print("An error occurred while attempting to load mappings.yaml")
+        print(f"Error message: {str(e)}")
         sys.exit(1)   
 
     configCore.update(mappingsCore) 
     return(configCore)
 
-GLOBALCONFIG = loadConfig()
+GLOBALCONFIG = loadConfigYaml()
+validateSiteConfigs()
