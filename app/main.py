1ce9085a08b2ae10a64e29db098ffd9ff9b8111e
Dennis Rand <rand@ecrime.dk>
2025-01-19 16:47:00 +0100

    Added Favicon for redoc

--- app/main.py
+++ app/main.py
@@ -351,6 +351,7 @@ async def redoc_html():
     return get_redoc_html(
         openapi_url=app.openapi_url,
         title=app.title + " - ReDoc",
+        redoc_favicon_url="/static/favicon.ico",  # Adding  favicon
         redoc_js_url="/static/redoc/redoc.standalone.js",
     )
 
